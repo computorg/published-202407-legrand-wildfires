68af1a1e083b782730c39f718732de629b1352d8
Julien Chiquet <julien.chiquet@inrae.fr>
2025-06-26 11:56:10 +0200

    README + sudo in setup

--- setup-env-ci.sh
+++ setup-env-ci.sh
@@ -1,1 +1,1 @@
-sudo apt -y update && apt install -y libudunits2-dev libgdal-dev libgeos-dev libproj-dev libsqlite3-dev libfontconfig1-dev libudunits2-dev libcurl4-openssl-dev
+sudo apt install -y libudunits2-dev libgdal-dev libgeos-dev libproj-dev libsqlite3-dev libfontconfig1-dev libudunits2-dev libcurl4-openssl-dev
